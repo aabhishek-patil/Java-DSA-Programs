ad21ae7ce1e26fe0f125156b6f70620908c95b0e
Abhishek Patil <143478051+aabhishek-patil@users.noreply.github.com>
2023-10-06 01:50:30 +0530

    Add files via upload

---
+++ Patterns/src/Pattern2.java
@@ -0,0 +1,13 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        pattern(4);
+    }
+    static void pattern(int n){
+        for(int row=1; row<=n; row++){
+            for(int col=1; col<=n-row+1; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
