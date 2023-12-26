4df50812c8ceed85545270550dfe6d945f8a737f
Abhishek Patil <143478051+aabhishek-patil@users.noreply.github.com>
2023-12-26 11:06:07 +0530

    Add files via upload

---
+++ Linked List/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+//        SLL list= new SLL(10);
+//        list.insertFirst(1);
+//        list.insertFirst(2);
+//        list.insertFirst(3);
+//        list.insertFirst(12);
+//
+//        list.insertLast(20);
+//        list.insert(100,3);
+//        list.Display();
+//        list.deleteFirst();
+//
+//
+//        list.Display();
+
+//        DLL list = new DLL();
+//        list.insertFirst(1);
+//        list.insertFirst(10);
+//        list.insertFirst(15);
+//        list.insert(10,20);
+//        list.insertFirst(2);
+//
+//
+//        list.display();
+//    }
+
+        CLL list = new CLL();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.delete(3);
+
+        list.display();
+
+    }
+}
